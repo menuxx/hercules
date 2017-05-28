b846829dfdae5c49c013662c54ee4daa9a06d5a5
yinchangsheng <ychangsheng@gmail.com>
2017-05-28 15:20:59 +0800

    调整开发环境可见参数重新发布代码

--- config.js
+++ config.js
@@ -1,27 +1,27 @@
 
 module.exports = {
-  appId: process.env.WX_3RD_APP_ID || 'wxb3d033d520d15fe7', // component_appid
-  appSecret: process.env.WX3RD__APP_SECRET || '99454ebe9eb76b704d9dfd8c34cc310b',  // component_appsecret
-  appKey: process.env.WX_3RD_APP_KEY || 'gh_e9100e798b39',
-  token: process.env.WX_3RD_APP_TOKEN || 'dg156c5719d3202f32a6619e14D0ccqd',
-  encodingAESKey: process.env.WX_3RD_ENCODING_AES_KEY || 'kat52d5719d320nEj2A6l19u14H21ct2aI8K08rltKl',
+  appId: process.env.WX_3RD_APP_ID, // component_appid
+  appSecret: process.env.WX_3RD_APP_SECRET,  // component_appsecret
+  appKey: process.env.WX_3RD_APP_KEY,
+  token: process.env.WX_3RD_APP_TOKEN,
+  encodingAESKey: process.env.WX_3RD_ENCODING_AES_KEY,
   // 缓存发布通知地址
   cachePublishPointers: process.env.CACHE_PUBLISH_POINTERS.split(',') || [],
   rabbit: {
-    uri: process.env.RABBIT_URI_01 || 'amqp://menuxx:28181820@wx3rd.menuxx.com:5692/menuxx'
+    uri: process.env.RABBIT_URI_01
   },
   auth: {
-    'username': process.env.AUTH_USERNAME || 'menuxx3rd',
-    'password': process.env.AUTH_PASSWORD || '28181820'
+    'username': process.env.AUTH_USERNAME,
+    'password': process.env.AUTH_PASSWORD
   },
   redis: {
-    port: process.env.REDIS_01_PORT || 7389,
-    host: process.env.REDIS_01_HOSTNAME || 'wx3rd.menuxx.com',
-    password: process.env.REDIS_01_PASSWORD || '4cj34t4j9'
+    port: process.env.REDIS_01_PORT,
+    host: process.env.REDIS_01_HOSTNAME,
+    password: process.env.REDIS_01_PASSWORD
   },
   leancloud: {
-    appId: process.env.LEANCLOUD_APPID || '98Yqw7EVpTpxvIP5wS4NK6j9-gzGzoHsz',
-    appKey: process.env.LEANCLOUD_APPKEY || 'HqvqMuGWLEKQjc02Dwzmz142'
+    appId: process.env.LEANCLOUD_APPID,
+    appKey: process.env.LEANCLOUD_APPKEY
   }
 }
 // [
