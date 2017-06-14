7ff36fda9fdba38be8fca2a9ead2608f74279db0
yinchangsheng <ychangsheng@gmail.com>
2017-06-14 18:16:40 +0800

    wxopenapi baseApi error return

--- wxopenapi/baseApi.js
+++ wxopenapi/baseApi.js
@@ -4,47 +4,49 @@ const {has} = require('lodash')
 
 module.exports = function (moduleName, baseApi) {
 
-  const {log, errorlog} = require('../logger')(moduleName)
-  
-  const get = function(apiUrl) {
-    var fullUrl = baseApi + apiUrl
-    log('weixin [%s] request url: %s, body: %o', moduleName, fullUrl)
-    return rp({
-      method: 'GET',
-      uri: fullUrl,
-      json: true
-    })
-    .then(function(res) {
-      if ( has(res, 'errcode') && has(res, 'errmsg')) {
-        return Promise.reject(res)
-      }
-      log('weixin [%s] response url: %s, body %o', moduleName, fullUrl, res)
-      return res
-    })
-    .catch(function(err) {
-      errorlog('wxapi [%s] error', moduleName, fullUrl, err)
-    })
-  }
+	const {log, errorlog} = require('../logger')(moduleName)
 
-  const post = function(apiUrl, body) {
-    var fullUrl = baseApi + apiUrl
-    log('weixin %s request url: %s, body: %o', moduleName, fullUrl, body)
-    return rp({
-      method: 'POST',
-      uri: fullUrl,
-      body: body,
-      json: true
-    })
-    .then(function(res) {
-      if ( has(res, 'errcode') && has(res, 'errmsg')) {
-          return Promise.reject(res)
-      }
-      log('weixin %s response url: %s, body: %o', moduleName, fullUrl, res)
-      return res
-    })
-    .catch(function(err) {
-      errorlog('wxapi [%s] error', moduleName, fullUrl, err)
-    })
-  }
-  return { post, get }
+	const get = function (apiUrl) {
+		var fullUrl = baseApi + apiUrl
+		log('weixin [%s] request url: %s, body: %o', moduleName, fullUrl)
+		return rp({
+			method: 'GET',
+			uri: fullUrl,
+			json: true
+		})
+			.then(function (res) {
+				if (has(res, 'errcode') && has(res, 'errmsg')) {
+					return Promise.reject(res)
+				}
+				log('weixin [%s] response url: %s, body %o', moduleName, fullUrl, res)
+				return res
+			})
+			.catch(function (err) {
+				errorlog('wxapi [%s] error', moduleName, fullUrl, err)
+				return err
+			})
+	}
+
+	const post = function (apiUrl, body) {
+		var fullUrl = baseApi + apiUrl
+		log('weixin %s request url: %s, body: %o', moduleName, fullUrl, body)
+		return rp({
+			method: 'POST',
+			uri: fullUrl,
+			body: body,
+			json: true
+		})
+			.then(function (res) {
+				if (has(res, 'errcode') && has(res, 'errmsg')) {
+					return Promise.reject(res)
+				}
+				log('weixin %s response url: %s, body: %o', moduleName, fullUrl, res)
+				return res
+			})
+			.catch(function (err) {
+				errorlog('wxapi [%s] error', moduleName, fullUrl, err)
+				return err
+			})
+	}
+	return {post, get}
 }
