aee3d15793c433c6eeb2fb6aea6cb5a048673e89
yinchangsheng <ychangsheng@gmail.com>
2017-08-18 12:43:38 +0800

    修复小问题

--- handler/apihandler.js
+++ handler/apihandler.js
@@ -38,17 +38,17 @@ route.put('/shop_wxlite/:appid/access_token_resume', function (req, resp) {
 	jsonAutoValid(req, resp).then(function () {
 		let {appid} = req.params;
 		// 获取该店铺的 access_token
-		console.log('00000000000000')
+		console.log('00000000000000', appid)
 		return wxlite.getAuthorizerInfo(appid).then(function ({authorizer_refresh_token}) {
-			console.log('1111111111111')
+			console.log('1111111111111', authorizer_refresh_token)
 			return wxlite.getAuthorizerAccessToken(appid, authorizer_refresh_token).then(function ({authorizer_access_token}) {
-				console.log('222222222222')
+				console.log('222222222222', authorizer_access_token)
 				return authorizerCache.getAuthorizerInfo(appid).then(function (cache_data) {
-					console.log('333333333333')
+					console.log('333333333333', cache_data)
 					cache_data.authorizer_access_token = authorizer_access_token
 					// 存放到 redis 中
 					return authorizerCache.putAuthorizerInfo(appid, cache_data).then(function () {
-						console.log('4444444444444')
+						console.log('4444444444444', cache_data)
 						return cache_data
 					})
 				})
