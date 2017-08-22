757dd90c094c927d43633ca7d004cd5d6cc99105
yinchangsheng <ychangsheng@gmail.com>
2017-08-22 16:25:07 +0800

    合并活动到大店版

--- test/code_test.js
+++ test/code_test.js
@@ -28,14 +28,37 @@ const {toggleVisible} = require('../service')
  //wx984ee7ec5f76f9cf
  //1409172302
 
+// wxcodeApi.saveCode({
+// 	gitBranch: 'tpl_single_01',
+// 	gitTag: 'tpl_single_0_1_22',
+// 	gitUrl: 'https://git.coding.net/yin80871901/menuxx-wxlite.git',
+// 	version: 'v0.1.22s',
+// 	templateId: 36,
+// 	templateType: 1,
+// 	desc: '1.添加面价活动 2.修复多出错误 3.增强稳定性',
+// 	_config: {
+// 		apiBaseurl: 'https://dev.api.menuxx.com/',
+// 		cdnBaseurl: 'https://file.menuxx.com',
+// 		fundebugApikey: '337b61f714201cbc40911fcb17a19a16e54399e26d68497f0a8681a999c66909'
+// 	},
+// 	_domains: {
+// 		requestDomains: ['https://dev.api.menuxx.com', 'https://api.menuxx.com', 'https://menuxx-xcx-log.wilddogio.com', 'https://fundebug.com'],
+// 		wsRequestDomains: ['wss://dev.message.menuxx.com', 'wss://message.menuxx.com'],
+// 		uploadDomains: ['https://file.menuxx.com'],
+// 		downloadDomains: ['https://file.menuxx.com']
+// 	}
+// }).then(function (res) {
+// 	console.log(res.id)
+// })
+
 wxcodeApi.saveCode({
-	gitBranch: 'tpl_single_01',
-	gitTag: 'tpl_single_0_1_19',
+	gitBranch: 'tpl_large_single_02',
+	gitTag: 'tpl_large_single_0_2',
 	gitUrl: 'https://git.coding.net/yin80871901/menuxx-wxlite.git',
-	version: 'v0.1.2ls',
-	templateId: 30,
-	templateType: 1,
-	desc: '1. 修复首页费用计算的歧义 2. 修复清除购物车时候的错误',
+	version: 'v0.1.22ls',
+	templateId: 37,
+	templateType: 3,
+	desc: '1.添加面价活动 2.修复多出错误 3.增强稳定性',
 	_config: {
 		apiBaseurl: 'https://dev.api.menuxx.com/',
 		cdnBaseurl: 'https://file.menuxx.com',
@@ -43,7 +66,7 @@ wxcodeApi.saveCode({
 	},
 	_domains: {
 		requestDomains: ['https://dev.api.menuxx.com', 'https://api.menuxx.com', 'https://menuxx-xcx-log.wilddogio.com', 'https://fundebug.com'],
-		wsRequestDomains: ['wss://dev.message.menuxx.com', 'wss://message.menuxx.com'],
+		wsRequestDomains: ['wss://menuxx_debug.mqtt.iot.gz.baidubce.com:8884', 'wss://dev.message.menuxx.com', 'wss://message.menuxx.com'],
 		uploadDomains: ['https://file.menuxx.com'],
 		downloadDomains: ['https://file.menuxx.com']
 	}
@@ -52,36 +75,13 @@ wxcodeApi.saveCode({
 })
 
 // wxcodeApi.saveCode({
-// 	gitBranch: 'tpl_large_single_02',
-// 	gitTag: 'tpl_large_single_0_2',
-// 	gitUrl: 'https://git.coding.net/yin80871901/menuxx-wxlite.git',
-// 	version: 'v0.1.19ls',
-// 	templateId: 27,
-// 	templateType: 3,
-// 	desc: '1.修复首页外卖提示文案',
-// 	_config: {
-// 		apiBaseurl: 'https://dev.api.menuxx.com/',
-// 		cdnBaseurl: 'https://file.menuxx.com',
-// 		fundebugApikey: '337b61f714201cbc40911fcb17a19a16e54399e26d68497f0a8681a999c66909'
-// 	},
-// 	_domains: {
-// 		requestDomains: ['https://dev.api.menuxx.com', 'https://api.menuxx.com', 'https://menuxx-xcx-log.wilddogio.com', 'https://fundebug.com'],
-// 		wsRequestDomains: ['wss://menuxx_debug.mqtt.iot.gz.baidubce.com:8884', 'wss://dev.message.menuxx.com', 'wss://message.menuxx.com'],
-// 		uploadDomains: ['https://file.menuxx.com'],
-// 		downloadDomains: ['https://file.menuxx.com']
-// 	}
-// }).then(function (res) {
-// 	console.log(res.id)
-// })
-
-// wxcodeApi.saveCode({
 // 	gitBranch: 'tpl_platform_01',
-// 	gitTag: 'tpl_platform_0_1_18',
+// 	gitTag: 'tpl_platform_0_1_21',
 // 	gitUrl: 'https://git.coding.net/yin80871901/menuxx-wxlite.git',
-// 	version: 'v0.1.18p',
-// 	templateId: 26,
+// 	version: 'v0.1.21p',
+// 	templateId: 35,
 // 	templateType: 2,
-// 	desc: '1.优化首页加载效率 2.修复首页菜单错乱问题 优化一下bug 3.首页交互优化',
+// 	desc: '1.添加面价活动 2.修复多出错误 3,增强稳定性',
 // 	_config: {
 // 		apiBaseurl: 'https://dev.api.menuxx.com/',
 // 		cdnBaseurl: 'https://file.menuxx.com',
